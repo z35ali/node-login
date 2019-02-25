c12572d138f71d666f4e75d026828f27af47e82f
Zafar <zafar.a.ali2@gmail.com>
2019-02-25 14:23:23 -0500

    MongoDB setup

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
+// DB Config
+const db = require('./config/keys').MongoURI;
+
+//Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 //EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
