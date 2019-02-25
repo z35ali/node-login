7f8e8db44e420830b8694c9028f58a16590f3601
Zafar <zafar.a.ali2@gmail.com>
2019-02-25 14:00:14 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+//Routes
+app.use('/', require('./routes/index'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server Started On Port ${PORT}`));
+
