40bf3711bc79dd91682920c38df041e2b0884787
Zafar <zafar.a.ali2@gmail.com>
2019-02-25 14:12:54 -0500

    General Layout

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 //Routes
 app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server Started On Port ${PORT}`));
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', (req, res) => res.send('Login'));
+//Login Page
+router.get('/login', (req, res) => res.render('login'));
+
+//Register Page
+router.get('/register', (req, res) => res.render('register'));
 
 module.exports = router;
