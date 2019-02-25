eb6a049d07bf3f86b92bb8c1178b832ea466d129
Zafar <zafar.a.ali2@gmail.com>
2019-02-25 14:57:44 -0500

    Register page

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,67 @@
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
+//User model
+const User = require('../models/User');
 //Login Page
 router.get('/login', (req, res) => res.render('login'));
 
 //Register Page
 router.get('/register', (req, res) => res.render('register'));
+
+//Register Handle
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    //Check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please fill in all fields' });
+    }
+
+    if (password !== password2) {
+        errors.push({ msg: 'Passwords do not match' });
+    }
+
+    // Check pass length
+    if (password.length < 6) {
+        errors.push({ msg: 'Password should be at least 6 characters' });
+
+    }
+
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    } else {
+        //Validation Passed
+        User.findOne({ email: email })
+            .then(user => {
+                if (user) {
+                    errors.push({ msg: 'Email is already registered' });
+                    //User exists
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+                }
+            });
+
+    }
+});
 
 module.exports = router;
