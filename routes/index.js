676d99a691e397c2cb4170d260dea95cf012533d
Zafar <zafar.a.ali2@gmail.com>
2019-02-25 20:55:11 -0500

    Bug fixed where /dashboard is accessible without login

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 // Welcome Page
 router.get('/', (req, res) => res.render('welcome'));
 
 //Dashboard
-router.get('/dashboard', (req, res) => res.render('dashboard'));
+router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard'));
 
 module.exports = router;
