d48a4f447133d59b657b2333ec3f443dbdc7afe4
Zafar <zafar.a.ali2@gmail.com>
2019-02-25 21:01:06 -0500

    Name appears on dashboard

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ const { ensureAuthenticated } = require('../config/auth');
 // Welcome Page
 router.get('/', (req, res) => res.render('welcome'));
 
-//Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard'));
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+    res.render('dashboard', {
+        user: req.user
+    })
+);
 
 module.exports = router;
