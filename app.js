2351eff55f997fdfdaae8bb645700b7ee0407718
Zafar <zafar.a.ali2@gmail.com>
2019-03-19 20:19:13 -0400

    Updated app.js with port

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ mongoose
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+
 // Express body parser
 app.use(express.urlencoded({ extended: true }));
 
@@ -60,5 +61,5 @@ app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
 const PORT = process.env.PORT || 5000;
-
+app.set('port', '${PORT}');
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
